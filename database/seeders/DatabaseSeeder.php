ddb92a4eadacd718941d1ae1ea4c8329e6bd360c
Rui Costa <rui.costa@inovador.net>
2025-10-25 12:11:43 +0100

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,13 +10,9 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
         ]);
     }
 }
